38e7e648032b070dd1b8f28d9b808658c6b04e28
Tonmecool <qpwoei.86@mail.ru>
2024-02-23 02:14:36 +0700

    .

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -14,13 +14,13 @@ func UserRoute(app *fiber.App) {
 
 	app.Post("/user", controllers.CreateUser)
 
+	app.Get("/users", controllers.GetAllUsers)
+
 	app.Get("/user/:userId", controllers.GetAUser)
 
 	app.Put("/user/:userId", controllers.EditAUser)
 
 	app.Delete("/user/:userId", controllers.DeleteAUser)
-
-	app.Get("/users", controllers.GetAllUsers)
 
 }
 
